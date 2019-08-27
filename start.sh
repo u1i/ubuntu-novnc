f9c71fa1df7363f60fc5251bee52bda2b45edb7b
Uli Hitzel <uli.hitzel@gmail.com>
2019-08-27 09:27:12 +0800

    cleanup

--- start.sh
+++ start.sh
@@ -5,6 +5,5 @@ then
     vncpassword="insecure"
 fi
 
-#printf "axway99\naxway99\n\n" | vncserver :1
 printf "$vncpassword\n$vncpassword\n\n" | vncserver :1
 /.novnc/utils/launch.sh --vnc localhost:5901 --listen 8080
